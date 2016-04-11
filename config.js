9ae86daab709c6a83d601e6bf01636029a535c31
TAC <tac@tac42.net>
2016-04-12 07:53:23 +0900

    Add header menu

---
+++ config.js
@@ -0,0 +1,8 @@
+
+var config = {
+    app_name: "WitchCoder",
+    welcome_msg: "Welcome to WitchCoder.",
+    app_desc: "WitchCoder is online judge platform."
+}
+
+module.exports = config;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var config = require('../config');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var name = req.user && req.user['name'] || "NotLogin"
-  res.render('index', { title: 'Express', name: name});
+  var user_name = req.user && req.user['name']
+  res.render('index', {
+    title:       config['app_name'],
+    welcome_msg: config['welcome_msg'],
+    app_desc:    config['app_desc'],
+    user_name:   user_name
+  });
 });
 
 module.exports = router;
