d932a5b4bf6c88bb54af5f6dbd8ace8a54dc5e37
TAC <tac@tac42.net>
2016-04-25 07:56:18 +0900

    Fix frontend app name

--- public/javascripts/app_angular.js
+++ public/javascripts/app_angular.js
@@ -1,15 +1,14 @@
-var App = angular.module("testapp", [])
+var App = angular.module("witch_coder", [])
 
 App.config(function(){
 
 })
 
-function testCtrl($timeout, $http){
+function index($timeout, $http){
   var t = this;
-  t.name = "name";
   $http.get('/api/users/count').then(function(res){
     t.count = res.data.user_count
   })
 }
 
-App.controller("testCtrl", ['$timeout', '$http', testCtrl])
+App.controller("index", index)
