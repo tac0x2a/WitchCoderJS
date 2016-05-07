afd4ede2e31e817b3e62f860eeec6f46c1187c75
TAC <tac@tac42.net>
2016-05-08 03:17:11 +0900

    Add registering new problem DB

--- routes/problem.js
+++ routes/problem.js
@@ -14,4 +14,35 @@ router.get('/new', function(req, res, next){
   return res.render('problem')
 });
 
+router.post('/new', function(req, res, next){
+  var title    = req.body.title
+  var problem  = req.body.problem
+  var input    = req.body.input
+  var expected = req.body.expected
+
+  if(typeof input === 'string'){
+    input = [input]
+    expected = [expected]
+  }
+
+  var judge = new Array()
+  for(var i = 0; i < input.length; i++){
+    judge.push({input:input[i], expected:expected[i]});
+  }
+
+  Problem.create({
+    title:    title,
+    question: problem,
+    owner: "me",
+    judge: judge
+  },
+  function(err, problem){
+    if(err) {
+      return res.render('problem', {title: title, problem: problem});
+    }
+    return res.redirect('/problem')
+  });
+});
+
+
 module.exports = router;
