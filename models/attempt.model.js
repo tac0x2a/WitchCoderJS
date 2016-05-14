7bd1030062f12acc67f05d6d40640ff1f617e960
TAC <tac@tac42.net>
2016-05-14 11:20:30 +0900

    Add attempt schema

---
+++ models/attempt.model.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+var uniqueValidator = require('mongoose-unique-validator');
+var Schema   = mongoose.Schema;
+
+var AttemptSchema = new Schema({
+  problem: {type: Schema.Types.ObjectId, required: true, ref: 'Problem'},
+  player:  {type: Schema.Types.ObjectId, required: true, ref: 'User'},
+  language:{type: String, required: true},
+  judge: [{
+    actual:  {type: String, required: true}, // stdout.
+    time_ms: {type: Number, required: true}, //
+    result:  {type: String, required: true}  // Judging, Accepted, Wrong, TimeExceeded, MemoryExceeded, RuntimeError, CompileError or InternalError
+  }],
+  submited:       { type: Date, default: Date.now },
+  judge_finished: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model("Attempt", AttemptSchema);
